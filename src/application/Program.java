1275b68d692788e29a75d8ef37981ae048684782
Pedro Santos <pedrohssantos1509@gmail.com>
2025-08-25 18:27:04 -0300

    feat: add findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
 
@@ -16,5 +17,10 @@ public class Program {
         System.out.println("==== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+
+        System.out.println("\n==== TEST 2: seller findById ====");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        list.forEach(System.out::println);
     }
 }
